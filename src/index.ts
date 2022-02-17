d4ccfe92602bc337095c6c7ac8ea30d3bfe1c5e0
Aaron Carneiro <github@aaroncarneiro.com>
2022-02-17 14:50:17 -0300

    feat: added root route

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,27 @@ api.use(cors());
 
 api.use(express.json());
 
+api.get("/", (req, res) => {
+  const page = `
+    <!DOCTYPE html>
+    <html lang="pt_br">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Projects API</title>
+    </head>
+    <body>
+      <h1>Projects API</h1>
+      <p>
+        Para buscar todos os projetos acesse <a href="/projects">/projects</a>, 
+        para buscar um projeto específico envie um body com o <code>name</code> do projeto.
+      </p>
+    </body>
+    </html>
+  `;
+  res.send(page);
+});
+
 api.get("/projects", async (req, res) => {
   const service: ProjectService = ProjectService.getInstance();
   const projects = await service.getAll();
@@ -21,7 +42,9 @@ api.get("/projects", async (req, res) => {
 
   if (name) {
     const project = projects.find((item) => item.url.includes(name));
-    if (project) { return res.json(project); }
+    if (project) {
+      return res.json(project);
+    }
     return res.status(404).json({ message: "Project not found" });
   } else {
     res.status(200).json(projects);
